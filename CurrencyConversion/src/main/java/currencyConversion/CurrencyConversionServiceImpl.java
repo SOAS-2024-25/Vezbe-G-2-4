d7b1b36ea311f8be52a6f98da801a8e233ea8a50
vffic <vffic@Ultima>
2025-07-10 17:44:40 +0200

    Vezbe 3 - 10.07.2025. - Global Exception Handling

--- CurrencyConversion/src/main/java/currencyConversion/CurrencyConversionServiceImpl.java
+++ CurrencyConversion/src/main/java/currencyConversion/CurrencyConversionServiceImpl.java
@@ -2,7 +2,6 @@ package currencyConversion;
 
 import java.math.BigDecimal;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 import api.dtos.CurrencyConversionDto;
 import api.dtos.CurrencyExchangeDto;
 import api.services.CurrencyConversionService;
+import util.exceptions.InvalidQuantityException;
 
 @RestController
 public class CurrencyConversionServiceImpl implements CurrencyConversionService {
@@ -18,10 +18,14 @@ public class CurrencyConversionServiceImpl implements CurrencyConversionService 
 	
 	@Override
 	public ResponseEntity<?> getConversion(String from, String to, BigDecimal quantity) {
-		String endPoint = "http://localhost:8000/currency-exchange?from=" + from + "&to=" + to;
-		ResponseEntity<CurrencyExchangeDto> response = template.getForEntity(endPoint, CurrencyExchangeDto.class);
+		if(quantity.compareTo(BigDecimal.valueOf(300.0)) == 1) {
+			throw new InvalidQuantityException(String.format("Quantity of %s is too large", quantity));
+		}
 		
-		if(response.getStatusCode() == HttpStatus.NOT_FOUND) return new ResponseEntity("Unable to fetch exchange", HttpStatus.BAD_REQUEST);
+		String endPoint = "http://localhost:8000/currency-exchange?from=" + from + "&to=" + to;
+		ResponseEntity<CurrencyExchangeDto> response;
+		response = template.getForEntity(endPoint, CurrencyExchangeDto.class);
+	
 		
 		return ResponseEntity.ok(new CurrencyConversionDto(response.getBody(), quantity));
 	}
