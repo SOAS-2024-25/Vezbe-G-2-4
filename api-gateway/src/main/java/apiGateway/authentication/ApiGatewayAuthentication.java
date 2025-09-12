cac727458f89583fcde923898d1b03f647dc52fd
VladimirFabri98 <vfficial98@gmail.com>
2025-09-12 09:32:07 +0200

    Vežbe 7 - Podešavanje za Docker

--- api-gateway/src/main/java/apiGateway/authentication/ApiGatewayAuthentication.java
+++ api-gateway/src/main/java/apiGateway/authentication/ApiGatewayAuthentication.java
@@ -34,7 +34,11 @@ public class ApiGatewayAuthentication {
 	
 	@Bean
 	ReactiveUserDetailsService reactiveUserDetailsService(WebClient.Builder webClientBuilder, BCryptPasswordEncoder encoder) {
-		WebClient client = webClientBuilder.baseUrl("http://localhost:8770").build();
+//		WebClient client = webClientBuilder.baseUrl("http://localhost:8770").build();
+
+//				Verzija za Docker
+		WebClient client = webClientBuilder.baseUrl("http://users-service:8770").build();
+		
 		
 		return user -> client.get()
 				.uri(uriBuilder -> uriBuilder
